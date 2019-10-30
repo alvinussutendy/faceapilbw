e70b5cc11759695f1e87b8fa4514b35b38921b47
Alvinus Sutendy <alvinussutendy@gmail.com>
2019-10-30 22:24:03 +0700

    First commit

---
+++ CobaWebcam2/src/cobawebcam2/CobaWebcam2.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cobawebcam2;
+
+import org.opencv.highgui.VideoCapture;
+import org.opencv.*;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CobaWebcam2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        VideoCapture cap  = new VideoCapture(0);
+        
+        if(!cap.isOpened()){
+            System.out.println("Cant connect to camera");
+        }
+        else{
+            System.out.println("Found webcam : "+cap.toString());
+            Mat frame = new Mat();
+            cap.retrieve(frame);
+            
+            Highgui.imwrite("Testing.jpg",frame);
+            cap.release();
+        }
+    }
+    
+}
