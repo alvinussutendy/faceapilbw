2093d3cc787a1afa26e18b7d1846e86f7f152f65
Johanes Irwan <johanes.frederic@gmail.com>
2019-11-07 10:51:44 +0700

    tambah foto

--- FaceApi/src/faceapi/FaceApi.java
+++ FaceApi/src/faceapi/FaceApi.java
@@ -64,7 +64,7 @@ public class FaceApi {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat src = imread("camera.jpg");*/
 //        String imageWithFaces = src.toString(); 
-        BufferedImage bImage = ImageIO.read(new File("Foto training\\cowo\\"+sc.nextLine()));
+        BufferedImage bImage = ImageIO.read(new File("Foto training\\cowo\\done\\"+sc.nextLine()));
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         ImageIO.write(bImage, "jpg", bos );
         byte [] data = bos.toByteArray();
